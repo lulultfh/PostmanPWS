8ff5444c7613382e8bc0b0cd821dc157cbc6f88f
lululthfh <punyalulu05@gmail.com>
2025-07-30 14:34:44 +0700

    feat: add route for cinta page

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const PORT = 3000;
 
 app.get('/test', (req, res) => {
-  res.send('Berhasil akses GET /test');
+  res.status(200).json({message: 'GET /test berhasil dijalankan'});
 });
+
+app.get('/cinta', (req,res) => {
+    res.send('No Cinta')
+})
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
